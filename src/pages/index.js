50e88414cdc4a575f8602b8a9f704c74ee806ef5
Ramunas <dev.nognys@gmail.com>
2021-06-10 06:14:46 +0300

    component update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
-import Layout from "../components/Layout";
+import Layout from "../components/layout";
 import Header from "../components/Header";
 import "../styles/init.scss";
 
